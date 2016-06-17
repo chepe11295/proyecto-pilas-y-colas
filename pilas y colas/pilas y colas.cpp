43c16c594637338e12b42404c02d293f503b7914
Jose Marroquin <josemarroquin_10@hotmail.com>
2016-06-16 20:36:17 -0600

    proyecto final pilas y colas

---
+++ pilas y colas/pilas y colas.cpp
@@ -0,0 +1,154 @@
+//============================================================================
+// Name        : pilas.cpp
+// Author      : Jose Marroquin
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <iostream>
+#include <cstdlib>
+#include "cola.h"
+#include "pila.h"
+
+using namespace std;
+
+int main() {
+
+// este codigo es como usted comento que iba a revisar nuestro proyecto
+
+
+	//inicio proceso cola
+	cout<<"\n\n\n<<<Proceso de Cola>>>"<<endl;
+	Cola cl;
+
+	cl.Listavaciacola(); // indica si la lista esta vacia o contiene datos
+	cl.tamaniocola();  //indica el tamanio de la lista
+
+	cl.Pushcola("jo"); 	//prueba que no permite mas o menos de 1 caracter en la cola
+	cl.Pushcola("j");
+	cl.Pushcola("o");
+	cl.Pushcola("s");
+	cl.Pushcola("e");	 //ingreso de caracteres al programa con la funcion push
+
+	cl.tamaniocola();  //indica el tamanio de la lista
+	cl.Listavaciacola();  // indica si la lista esta vacia o contiene datos
+
+	cl.Imprimircola();	 //imprimir todos los nodos existentes hasta el momento
+	cl.Topcola(); 		//imprimir el caracter que se encuentra en top en este momento
+	cl.Popcola(); 		//eliminar el caracter que se encuentra en top en este momento
+	cl.Imprimircola();
+	cl.tamaniocola();  //indica el tamanio de la lista
+	cl.Topcola();
+	cl.Popcola();
+	system("PAUSE");
+	//fin proceso cola
+
+	//inicio proceso pila
+	cout<<"\n\n\n<<<Proceso de Pila>>>"<<endl;
+	Pila pl;
+	pl.Listavaciapila(); // indica si la lista esta vacia o contiene datos
+	pl.tamaniopila();  //indica el tamanio de la lista
+
+	pl.Pushpila("j");
+	pl.Pushpila("o");
+	pl.Pushpila("s");
+	pl.Pushpila("e"); 	//ingreso de caracteres al programa con la funcion push
+	pl.Pushpila("jo"); 	//prueba que no permite mas o menos de 1 caracter en la pila
+
+	pl.Listavaciapila(); // indica si la lista esta vacia o contiene datos
+	pl.tamaniopila();  //indica el tamanio de la lista
+
+	pl.Imprimirpila();	//imprimir todos los nodos existentes hasta el momento
+	pl.tamaniopila();  //indica el tamanio de la lista
+	pl.Toppila();		//imprimir el caracter que se encuentra en top en este momento
+	pl.Poppila();		//eliminar el caracter que se encuentra en top en este momento
+	pl.Imprimirpila();
+	pl.tamaniopila();  //indica el tamanio de la lista
+	pl.Toppila();
+	pl.Poppila();
+
+	//fin proceso pila
+
+	/*menu para ejecutar funciones
+
+	string caracter;
+	Cola cl;
+	Pila pl;
+	int opcion;
+	while(opcion!=13)
+	{
+		cout<<"\n\n\n<<<Proceso de pilas y colas>>>"<<endl;
+		cout<<"opciones para cola: "<<endl;
+		cout<<"1. push. "<<endl;
+		cout<<"2. pop. "<<endl;
+		cout<<"3. top. "<<endl;
+		cout<<"4. imprimir. "<<endl;
+		cout<<"5. tamaño de lista. "<<endl;
+		cout<<"6. verificar si hay datos en lista.\n"<<endl;
+		cout<<"opciones para pila: "<<endl;
+		cout<<"7. push. "<<endl;
+		cout<<"8. pop. "<<endl;
+		cout<<"9. top. "<<endl;
+		cout<<"10. imprimir. "<<endl;
+		cout<<"11. tamaño de lista. "<<endl;
+		cout<<"12. verificar si hay datos en lista.\n\n"<<endl;
+		cout<<"13. salir.\n\n"<<endl;
+		cout<<"ingrese opcion que desea realizar: "<<endl;
+		cin>>opcion;
+
+		switch(opcion)
+		{
+		case 1:
+			cout<<"ingrese caracter que desea ingresar: "<<endl;
+			cin>>caracter;
+			cl.Pushcola(caracter);
+			break;
+		case 2:
+			cl.Popcola();
+			break;
+		case 3:
+			cl.Topcola();
+			break;
+		case 4:
+			cl.Imprimircola();
+			break;
+		case 5:
+			cl.tamaniocola();
+			break;
+		case 6:
+			cl.Listavaciacola();
+			break;
+		case 7:
+			cout<<"ingrese caracter que desea ingresar: "<<endl;
+			cin>>caracter;
+			pl.Pushpila(caracter);
+			break;
+		case 8:
+			pl.Poppila();
+			break;
+		case 9:
+			pl.Toppila();
+			break;
+		case 10:
+			pl.Imprimirpila();
+			break;
+		case 11:
+			pl.tamaniopila();
+			break;
+		case 12:
+			pl.Listavaciapila();
+			break;
+		case 13:
+			cout<<"\n HASTA LA PROXIMA"<<endl;
+			break;
+		default:
+			cout<<"esta opcion no esta disponible"<<endl;
+			cout<<"por favor, ingrese una opcion entre 1 y 12"<<endl;
+
+	}
+	system("PAUSE");
+	}
+	*
+	 */
+
+}
---
+++ pilas y colas/src/pila.h
@@ -0,0 +1,141 @@
+/*
+ * pila.h
+ *
+ *  Created on: 6/06/2016
+ *      Author: Mario Rosa
+ */
+
+#ifndef PILA_H_
+#define PILA_H_
+
+
+#include <iostream>
+using namespace std;
+
+
+struct Pila //estructura creada para crear vraiables en el programa principal
+{
+		struct Letra  //estructura creada para crear los nodos donde se van almacenar los caracteres
+		{
+			string caracter; //dato donde se almacenara el caracter
+			struct Letra *siguiente;
+		};
+
+		Letra *top = NULL;
+		Letra *auxiliar = NULL;
+		Letra *abajopila = NULL;                 // los 4 anterios son punteros para manipular los datos en las funciones que estan mas adelante
+
+
+			void Pushpila (string x)   //funcion push para insertar caracteres
+			{
+				int noletras = x.size(); // contador de letras que contiene el string ingresado en el programa principal
+				if (noletras == 1)      //condicion para que cree nuevos nodos solamente si el string ingresado tiene un caracter.
+				{
+				if(top==NULL)    //if para crear el primer nodo de la estructura cuando aun no tenemos algun dato
+					{
+						top = new(Letra);
+						top->caracter = x;
+						abajopila = top;
+						abajopila->siguiente = NULL;
+					}
+					else    //metodo para crear un nuevo nodo siempre y cuando ya exista por lo menos 1
+					{
+						auxiliar = new(Letra);
+						auxiliar->siguiente = top;
+						auxiliar->caracter = x;
+						top = auxiliar;
+					}
+				}
+				else    // mensaje de devolucion al usuario en caso de que el string ingresado contenga mas o menos de una letra.
+				{
+				cout<<"el valor: ''"<< x << "'' que ingresó, no puede ser almacenado porque contiene mas de 1 caracter."<<endl;
+				cout<<"se procederá con las instrucciones siguientes ordenadas por ti"<<endl;
+				cout<<"------------------------------------------------------------------------------------------------\n\n";
+				}
+			}
+
+			void Toppila () //funcion top para mostrar el dato que esta de primero en los datos almacenados.
+			{
+				if(top==NULL) //mensaje de devolucion en caso de que aun no exista algun nodo
+				{
+					cout<<"en este momento no existe la estructura"<<endl;
+				}
+				else // imprime el caracter que se encuentra en el puntero top
+				{
+					cout<<"\t el top de la Pila es: "<<top->caracter<<endl;
+
+				}
+			}
+
+			void Poppila() //metodo para eliminar el caracter que esta en el puntero top
+			{
+				if(top == NULL) //mensaje de devolucion en caso de que aun no exista algun nodo
+				{
+					cout<<"en este momento No existe ninguna estructura"<<endl;
+				}
+				else //asigna top al nodo que se encuentra siguiente de el y elimina el caracter que se encontraba en top anteriormente.
+				{
+				auxiliar = top; //aqui un puntero auxiliar pasa a ser top para mantener guardado ese caracter
+				top = top->siguiente; // aqui top para a ser el nodo que se encuentra siguiente a él
+				delete (auxiliar); //aqui se borra el caracter que estaba en top antes de que top pasara a ser el nodo sigiuente a él
+				}
+			}
+
+			void Imprimirpila() // metodo para mostrar todos los datos que se encuentran en la estructura.
+			{
+				auxiliar = top;
+				int i = 0;//contador para enumerar los nodos
+				if (top == NULL)//mensaje de devolucion en caso de que aun no exista algun nodo
+				{
+					cout<<"en este momento No existe una estructura"<<endl;
+				}
+				else // proceso en caso de que si existan nodos para imprimir
+				{
+					while (auxiliar != NULL)// proceso para que imprima todos caracteres contenidos en los nodos existentes
+					{
+						i++; //aumentar i para imprimir el numero que corresponde
+						cout<<i<<". "<<auxiliar->caracter<<endl;
+						auxiliar = auxiliar->siguiente;
+					}
+				}
+			}
+
+			void Listavaciapila() // funcion para verificar si la lista esta vacia
+			{
+			if(top == NULL)//mensaje para confirmar que la lista se encuentra vacia
+			{
+				cout <<"la lista se encuentra vacia"<<endl;
+				cout<<"-----------------------------\n\n\n";
+			}
+			else //mensaje para confirmar que la lista contiene datos
+			{
+				cout<<"la lista contiene datos"<<endl;
+				cout<<"-----------------------------\n\n\n";
+			}
+			}
+
+			void tamaniopila ()//funcion para verificar el tamanio de la lista
+			{
+				auxiliar = top;
+				int i = 0;
+				if(top==NULL) //mensaje de devolucion en caso de que aun no exista algun nodo
+				{
+					cout<<"El tamaño de la Lista de pila es: "<< i <<endl;
+					cout<<"-----------------------------\n\n\n";
+				}
+				else
+				{
+					while (auxiliar != NULL)// proceso para contar todos caracteres contenidos en los nodos existentes
+					{
+						i++; //aumentar i para contar el numero de caracteres existentes
+						auxiliar = auxiliar->siguiente;
+					}
+				cout<<"El tamaño de la Lista de pila es: "<< i <<endl;
+				cout<<"-----------------------------\n\n\n";
+				}
+
+			}
+
+};
+
+#endif /* PILA_H_ */
